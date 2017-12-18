f4a8d264b823cb8a2764c2c37b2165bcf562b98c
blobby <blobby@radico.ru>
2017-12-18 18:04:10 +0300

    доделал меню

--- sketch_nov03a_mega128gyro/menu.h
+++ sketch_nov03a_mega128gyro/menu.h
@@ -143,7 +143,7 @@ class MyMenu {
 
       link_back_next(&node_lf_set_max, &node_lf_set_min);
 
-      node_rf_weel.name = "Left Last Weel";
+      node_rf_weel.name = "Right First Weel";
       link_parent_child(&node_weel, &node_rf_weel);
       link_back_next(&node_lf_weel, &node_rf_weel);
 
@@ -155,37 +155,130 @@ class MyMenu {
       link_back_next(&node_rf_set_min, &node_rf_set_max);
 
       link_back_next(&node_rf_set_max, &node_rf_set_min);
-      
-      /*
-        MENU_NODE node_ll_weel;
-        MENU_NODE node_ll_set_min;
-        MENU_NODE node_ll_set_max;
-        MENU_NODE node_rl_weel;
-        MENU_NODE node_rl_set_min;
-        MENU_NODE node_rl_set_max;
-        MENU_NODE node_compressor;
-        MENU_NODE node_compressor_set_min;
-        MENU_NODE node_compressor_set_max;
-        MENU_NODE node_gyro;
-        MENU_NODE node_gyro_x;
-        MENU_NODE node_x_set_min;
-        MENU_NODE node_x_set_max;
-        MENU_NODE node_gyro_y;
-        MENU_NODE node_y_set_min;
-        MENU_NODE node_y_set_max;
-        MENU_NODE node_reset;
-        MENU_NODE node_reset_gyro_zerro;
-        MENU_NODE node_reset_weels;
-        MENU_NODE node_reset_compressor;
-        MENU_NODE node_hardware;
-        MENU_NODE node_hard_f_only;
-        MENU_NODE node_hard_l_only;
-        MENU_NODE node_hard_l_f;
-        MENU_NODE node_hard_compressor;
-        MENU_NODE node_hard_compressor_exist;
-        MENU_NODE node_hard_compressor_not_exist;
 
-      */
+      node_ll_weel.name = "Left Last Weel"
+      link_parent_child(&node_weel, &node_ll_weel);
+      link_back_next(&node_rf_weel, &node_ll_weel);
+
+      node_ll_set_min.name = "Set Min";
+      link_parent_child(&node_ll_weel, &node_ll_set_min);
+      
+      node_ll_set_max.name = "Set Max";
+      link_parent_child(&node_ll_weel, &node_ll_set_max);
+      link_back_next(&node_ll_set_min, &node_ll_set_max);
+
+      link_back_next(&node_ll_set_max, &node_ll_set_min);
+      
+      node_rl_weel.name = "Right Last Weel"
+      link_parent_child(&node_weel, &node_rl_weel);
+      link_back_next(&node_ll_weel, &node_rl_weel);
+
+      node_rl_set_min.name = "Set Min";
+      link_parent_child(&node_rl_weel, &node_rl_set_min);
+      
+      node_rl_set_max.name = "Set Max";
+      link_parent_child(&node_rl_weel, &node_rl_set_max);
+      link_back_next(&node_rl_set_min, &node_rl_set_max);
+
+      link_back_next(&node_rl_set_max, &node_rl_set_min);
+
+      link_back_next(&node_rl_weel, &node_lf_weel); // loop to lf
+
+      node_compressor.name = "Compressor";
+      link_parent_child(&node_calibrate, &node_compressor);
+      link_back_next(&node_weel, &node_compressor);
+
+      node_compressor_set_min.name = "Set Min";
+      link_parent_child(&node_compressor, &node_compressor_set_min);
+      
+      node_compressor_set_max.name = "Set Max";
+      link_parent_child(&node_compressor, &node_compressor_set_max);
+      link_back_next(&node_compressor_set_min, &node_compressor_set_max);
+
+      link_back_next(&node_compressor_set_max, &node_compressor_set_min);
+
+      node_gyro.name = "Gyroscope";
+      link_parent_child(&node_calibrate, &node_gyro);
+      link_back_next(&node_compressor, &node_gyro);
+
+      node_gyro_x.name = "X-axis";
+      link_parent_child(&node_gyro, &node_gyro_x);
+      
+      node_x_set_min.name = "Set Min";
+      link_parent_child(&node_gyro_x, &node_x_set_min);
+      
+      node_x_set_max.name = "Set Max";
+      link_parent_child(&node_gyro_x, &node_x_set_max);
+      link_back_next(&node_x_set_min, &node_x_set_max);
+
+      link_back_next(&node_x_set_max, &node_x_set_min);
+
+      node_gyro_y.name = "Y-axis";
+      link_parent_child(&node_gyro, &node_gyro_y);
+      link_back_next(&node_gyro_x, &node_gyro_y);
+      
+      node_x_set_min.name = "Set Min";
+      link_parent_child(&node_gyro_y, &node_y_set_min);
+      
+      node_x_set_max.name = "Set Max";
+      link_parent_child(&node_gyro_y, &node_y_set_max);
+      link_back_next(&node_y_set_min, &node_y_set_max);
+
+      link_back_next(&node_y_set_max, &node_y_set_min);
+
+      link_back_next(&node_gyro_y, &node_gyro_x); // loop to X-axis
+
+      link_back_next(&node_gyro, &node_weel); // loop to weel
+      
+      node_reset.name = "Reset";   
+      link_parent_child(&node_menu, &node_reset);
+      link_back_next(&node_calibrate, &node_reset); 
+
+      node_reset_gyro_zerro.name = "Reset gyro";
+      link_parent_child(&node_reset, &node_reset_gyro_zerro);
+
+      node_reset_weels.name = "Reset weels";
+      link_parent_child(&node_reset, &node_reset_weels);
+      link_back_next(&node_reset_gyro_zerro, &node_reset_weels);
+
+      node_reset_compressor.name = "Reset compressor";
+      link_parent_child(&node_reset, &node_reset_compressor);
+      link_back_next(&node_reset_weels, &node_reset_compressor);
+
+      link_back_next(&node_reset_compressor, &node_reset_gyro_zerro); // loop to reset gyro
+
+      node_hardware.name = "Hardware";
+      link_parent_child(&node_menu, &node_hardware);
+      link_back_next(&node_reset, &node_hardware); 
+
+      node_hard_f_only.name = "First Weels Only";
+      link_parent_child(&node_hardware, &node_hard_f_only);
+
+      node_hard_l_only.name = "Last Weels Only";
+      link_parent_child(&node_hardware, &node_hard_l_only);
+      link_back_next(&node_hard_f_only, &node_hard_l_only); 
+
+      node_hard_l_f.name = "4 x 4";
+      link_parent_child(&node_hardware, &node_hard_l_f);
+      link_back_next(&node_hard_l_only, &node_hard_l_f); 
+
+      node_hard_compressor.name = "Is compressor";
+      link_parent_child(&node_hardware, &node_hard_compressor);
+      link_back_next(&node_hard_l_f, &node_hard_compressor); 
+
+      node_hard_compressor_exist.name = "Compressor exists";
+      link_parent_child(&node_hard_compressor, &node_hard_compressor_exist);   
+
+      node_hard_compressor_not_exist.name = "Compressor NOT exists";
+      link_parent_child(&node_hard_compressor, &node_hard_compressor_not_exist);   
+
+      link_back_next(&node_hard_compressor_exist, &node_hard_compressor_not_exist); 
+      link_back_next(&node_hard_compressor_not_exist, &node_hard_compressor_exist); 
+
+      link_back_next(&node_hard_compressor, &node_hard_f_only); // loop to compressor
+
+      link_back_next(&node_hardware, &node_calibrate); // loop to colibrate
+
       root = &node_menu;
       current = root;
     };
