6744c4a1367a974c83d6e6175448cb0b3c6a0886
blobby <blobby@radico.ru>
2017-12-22 12:08:37 +0300

    изменил структуру файлов

--- sketch_nov03a_mega128gyro/f_40_I2C.ino
+++ sketch_nov03a_mega128gyro/f_40_I2C.ino
@@ -14,6 +14,7 @@
  Web      :  http://www.tkjelectronics.com
  e-mail   :  kristianl@tkjelectronics.com
  */
+#include <Wire.h>
 
 const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
 const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
